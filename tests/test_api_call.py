ba07a95c31271e4ab60cd37ad900f63f3eccae2d
Louis Maddox <louismmx@gmail.com>
2024-05-28 21:34:27 +0100

    test: example of using API call with a localhost factorial endpoint on port 80

--- tests/test_api_call.py
+++ tests/test_api_call.py
@@ -4,12 +4,16 @@ import httpolars as httpl
 
 
 def test_api_call():
-    df = pl.DataFrame({"number": 123})
-    result = df.with_columns(api_result=httpl.api_call("number").alias("is_even"))
+    df = pl.DataFrame({"number": [1, 2, 3]})
+    result = df.with_columns(
+        api_result=httpl.api_call(
+            "number", endpoint="http://localhost:80/permutations"
+        ).alias("factorial")
+    )
     expected_df = pl.DataFrame(
         {
-            "number": 123,
-            "is_even": [False],
+            "number": [1, 2, 3],
+            "factorial": [1, 2, 6],
         }
     )
     assert result.equals(expected_df)
