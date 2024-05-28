a8cf5a2f69bb9d6bf9062896fe8d88e953b4fcd5
Louis Maddox <louismmx@gmail.com>
2024-05-29 00:23:51 +0100

    test: unit tests pass for URL creation

--- tests/test_api_call.py
+++ tests/test_api_call.py
@@ -12,25 +12,24 @@ def test_api_call_noop():
     )
     assert result.to_dicts() == snapshot(
         [
-            {"number": 1, "response": "http://localhost:80/noop?param=1"},
-            {"number": 2, "response": "http://localhost:80/noop?param=2"},
-            {"number": 3, "response": "http://localhost:80/noop?param=3"},
+            {"number": 1, "response": "http://localhost:80/noop?number=1"},
+            {"number": 2, "response": "http://localhost:80/noop?number=2"},
+            {"number": 3, "response": "http://localhost:80/noop?number=3"},
         ]
     )
 
 
-@mark.skip
 def test_api_call_factorial():
     df = pl.DataFrame({"number": [1, 2, 3]})
     result = df.with_columns(
         response=httpl.api_call(
-            "number", endpoint="http://localhost:80/permutations"
-        ).alias("factorial")
+            "number", endpoint="http://localhost:80/factorial"
+        ).alias("permutations")
     )
-    expected_df = pl.DataFrame(
-        {
-            "number": [1, 2, 3],
-            "factorial": [1, 2, 6],
-        }
+    assert result.to_dicts() == snapshot(
+        [
+            {"number": 1, "response": "http://localhost:80/factorial?number=1"},
+            {"number": 2, "response": "http://localhost:80/factorial?number=2"},
+            {"number": 3, "response": "http://localhost:80/factorial?number=3"},
+        ]
     )
-    assert result.to_dicts() == expected_df.to_dicts()
