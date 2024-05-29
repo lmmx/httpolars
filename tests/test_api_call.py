655a70bf01a5945eaeb6ce302723608dcf8675d8
Louis Maddox <louismmx@gmail.com>
2024-05-29 09:05:32 +0100

    test: access field value in JSON

--- tests/test_api_call.py
+++ tests/test_api_call.py
@@ -7,29 +7,26 @@ import httpolars as httpl
 
 def test_api_call_noop():
     df = pl.DataFrame({"value": ["x", "y", "z"]})
-    result = df.with_columns(
-        response=httpl.api_call("value", endpoint="http://localhost:8000/noop")
-    )
+    response = httpl.api_call("value", endpoint="http://localhost:8000/noop")
+    result = df.with_columns(response.str.json_path_match("$.value"))
     assert result.to_dicts() == snapshot(
         [
-            {"value": "x", "response": '{"value":"x"}'},
-            {"value": "y", "response": '{"value":"y"}'},
-            {"value": "z", "response": '{"value":"z"}'},
+            {"value": "x"},
+            {"value": "y"},
+            {"value": "z"},
         ]
     )
 
 
 def test_api_call_factorial():
     df = pl.DataFrame({"number": [1, 2, 3]})
-    result = df.with_columns(
-        permutations=httpl.api_call(
-            "number", endpoint="http://localhost:8000/factorial"
-        )
-    )
+    response = httpl.api_call("number", endpoint="http://localhost:8000/factorial")
+    factorial = response.str.json_path_match("$.factorial").str.to_integer()
+    result = df.with_columns(permutations=factorial)
     assert result.to_dicts() == snapshot(
         [
-            {"number": 1, "permutations": '{"factorial":1}'},
-            {"number": 2, "permutations": '{"factorial":2}'},
-            {"number": 3, "permutations": '{"factorial":6}'},
+            {"number": 1, "permutations": 1},
+            {"number": 2, "permutations": 2},
+            {"number": 3, "permutations": 6},
         ]
     )
