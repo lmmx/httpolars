a6ed7b6ab37b5540a97fd72882bf9d37b86d03e7
Louis Maddox <louismmx@gmail.com>
2024-05-30 18:42:54 +0100

    feat: create a series of status codes from responses

--- src/api.rs
+++ src/api.rs
@@ -2,14 +2,16 @@ use reqwest::blocking::Client;
 use reqwest::Error;
 use std::collections::HashMap;
 
-pub fn make_request(endpoint: &String, params: &HashMap<&str, &str>) -> Result<String, Error> {
+pub fn make_request(endpoint: &String, params: &HashMap<&str, &str>) -> Result<(String, u16), Error> {
     let client = Client::new();
 
     let response = client
         .get(endpoint)
         .query(&params)
-        .send()?
-        .text()?;
+        .send()?;
+    
+    let status = response.status().as_u16();
+    let text = response.text()?;
 
-    Ok(response)
+    Ok((text, status))
 }
--- src/expressions.rs
+++ src/expressions.rs
@@ -72,54 +72,107 @@ fn api_call(inputs: &[Series], kwargs: ApiCallKwargs) -> PolarsResult<Series> {
     let name = s.name();
     let endpoint = &kwargs.endpoint;
 
-    let out = match s.dtype() {
+    let (response_texts, status_codes) = match s.dtype() {
+        // DataType::String => {
+        //     let ca = s.str()?;
+        //     StringChunked::from_iter(
+        //         ca.into_iter().map(|opt_v| opt_v.map(|v| {
+        //             let mut params = HashMap::new();
+        //             params.insert(name, v);
+        //             match make_request(endpoint, &params) {
+        //             	Ok((response_text, status_code)) => (response_text, status_code),
+        //                 Err(e) => (format!("Error: {}", e), 0),
+        //             }
+        //         })
+        //         ),
+        //     ).into_series()
+        // },
         DataType::String => {
             let ca = s.str()?;
-            StringChunked::from_iter(
-                ca.into_iter().map(|opt_v| opt_v.map(|v| {
+            let (texts, codes): (Vec<Option<String>>, Vec<Option<u32>>) = ca.into_iter().map(|opt_v| {
+                opt_v.map_or((None, None), |v| {
                     let mut params = HashMap::new();
                     params.insert(name, v);
                     match make_request(endpoint, &params) {
-                    	Ok(response) => response,
-                        Err(e) => format!("Error: {}", e),
+                        Ok((response_text, status_code)) => (Some(response_text), Some(status_code as u32)),
+                        Err(e) => (Some(format!("Error: {}", e)), Some(0)),
                     }
                 })
-                ),
-            ).into_series()
+            }).unzip();
+            (
+				StringChunked::from_iter(texts).into_series(),
+				UInt32Chunked::from_iter(codes.into_iter()).into_series()
+			)
         },
+        // DataType::Int32 => {
+        //     let ca = s.i32()?;
+        //     StringChunked::from_iter(
+        //         ca.into_iter().map(|opt_v| opt_v.map(|v| {
+        //             let mut params = HashMap::new();
+        //             let v_str = v.to_string();
+        //             params.insert(name, v_str.as_str());
+        //             match make_request(endpoint, &params) {
+        //             	Ok((response_text, status_code)) => (response_text, status_code),
+        //                 Err(e) => (format!("Error: {}", e), 0),
+        //             }
+        //         })
+        //         ),
+        //     ).into_series()
+        // },
         DataType::Int32 => {
             let ca = s.i32()?;
-            StringChunked::from_iter(
-                ca.into_iter().map(|opt_v| opt_v.map(|v| {
+            let (texts, codes): (Vec<Option<String>>, Vec<Option<u32>>) = ca.into_iter().map(|opt_v| {
+                opt_v.map_or((None, None), |v| {
                     let mut params = HashMap::new();
                     let v_str = v.to_string();
                     params.insert(name, v_str.as_str());
                     match make_request(endpoint, &params) {
-                    	Ok(response) => response,
-                        Err(e) => format!("Error: {}", e),
+                        Ok((response_text, status_code)) => (Some(response_text), Some(status_code as u32)),
+                        Err(e) => (Some(format!("Error: {}", e)), Some(0)),
                     }
                 })
-                ),
-            ).into_series()
+            }).unzip();
+            (
+				StringChunked::from_iter(texts).into_series(),
+				UInt32Chunked::from_iter(codes.into_iter()).into_series()
+			)
         },
+        // DataType::Int64 => {
+        //     let ca = s.i64()?;
+        //     StringChunked::from_iter(
+        //         ca.into_iter().map(|opt_v| opt_v.map(|v| {
+        //             let mut params = HashMap::new();
+        //             let v_str = v.to_string();
+        //             params.insert(name, v_str.as_str());
+        //             match make_request(endpoint, &params) {
+        //             	Ok((response_text, status_code)) => (response_text, status_code),
+        //                 Err(e) => (format!("Error: {}", e), 0),
+        //             }
+        //         })
+        //         ),
+        //     ).into_series()
+        // },
         DataType::Int64 => {
             let ca = s.i64()?;
-            StringChunked::from_iter(
-                ca.into_iter().map(|opt_v| opt_v.map(|v| {
+            let (texts, codes): (Vec<Option<String>>, Vec<Option<u32>>) = ca.into_iter().map(|opt_v| {
+                opt_v.map_or((None, None), |v| {
                     let mut params = HashMap::new();
                     let v_str = v.to_string();
                     params.insert(name, v_str.as_str());
                     match make_request(endpoint, &params) {
-                    	Ok(response) => response,
-                        Err(e) => format!("Error: {}", e),
+                        Ok((response_text, status_code)) => (Some(response_text), Some(status_code as u32)),
+                        Err(e) => (Some(format!("Error: {}", e)), Some(0)),
                     }
                 })
-                ),
-            ).into_series()
+            }).unzip();
+            (
+				StringChunked::from_iter(texts).into_series(),
+				UInt32Chunked::from_iter(codes.into_iter()).into_series()
+			)
         },
         dtype => polars_bail!(InvalidOperation:format!("Data type {dtype} not \
              supported for api_call, expected String, Int32, Int64.")),
     };
 
-    Ok(out.into_series())
+    Ok(response_texts.into_series())
 }
